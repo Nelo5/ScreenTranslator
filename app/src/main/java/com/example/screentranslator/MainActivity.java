1c286ee58ed5b4e357056338afcbfac1a08c98fc
Nelo5 <furmit@mail.ru>
2024-02-13 22:15:37 +0300

    444

--- app/src/main/java/com/example/screentranslator/MainActivity.java
+++ app/src/main/java/com/example/screentranslator/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.screentranslator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.loader.AssetsProvider;
 import android.os.Bundle;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.File;
 
@@ -14,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         File projectDir = getApplicationContext().getFilesDir();
         File newDir = new File(projectDir, "tessdata");
-        newDir.mkdirs();
+        FileOutputStream fos
+        TessBaseAPI tess = new TessBaseAPI();
+        String dataPath = new File(getFilesDir(), "tesseract").getAbsolutePath();
+        if (!tess.init(dataPath, "rus")) { // could be multiple languages, like "eng+deu+fra"
+            tess.recycle();
+            return;
+        }
+        new Thread(()-> {
+            tess.setImage();
+            String text = tess.getUTF8Text();
+            tess.recycle();
+        });
     }
 }
